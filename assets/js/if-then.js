35f473e0dc62b39388a38272edc0f3e941d28822
amaliehausmann <amaliehausmann@gmail.com>
2023-11-16 13:29:50 +0100

    løsning af opgave

--- assets/js/if-then.js
+++ assets/js/if-then.js
@@ -8,33 +8,56 @@ const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B.
 
-if ( /*..dit expression Her*/ ) {
+if(A==B) {
     console.log('statement true!');
+} else{
+    console.log('not true');
 }
 
 
 //opgave 2: skrive en If then statement der vurderer om A er større end B.
-
+if(A > B) {
+    console.log('A er større end B')
+   
+} else{
+        console.log('A er ikke større end b')
+}
 
 
 
 //opgave 3: Skriv en If then statement der vurderer om A er mindre end eller lig med B
-
+if(A<=B){
+    console.log('Statement true!')
+} else{
+    console.log('not true');
+}
 
 
 
 
 //opgave 4: Skriv en If then statement der bliver FALSE når du sammenligner B og C.
 
-
+if(B===C){
+console.log('True');
+} else{
+    console.log('False')
+}
 
 
 
 //opgave 5: Skriv en If then statement der bliver TRUE når du sammenligner B og C.
-
+if(B==C){
+    console.log('True');
+} else{
+    console.log(False);
+}
 
 
 
 //opgave 6: Skriv en If then statement der sammenligner B = C og E = A hvis B=C skal den blive TRUE. 
-
+if(B == C && E == A ){
+    console.log('true')
+} else{
+    console.log('false');
+}
 
--- assets/js/realworld.js
+++ assets/js/realworld.js
@@ -12,18 +12,36 @@ const myAnsverInput = document.getElementById('answerOne');
 const myAnsverButton = document.getElementById('answerButton');
 const myAnsverFeedbackElement = document.getElementById('answerFeedback');
 
-myAnsverButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  /* din kode her. Skriv en alert der skriver "du har svarret rigtigt" hvis svarret er true
-  og "du har svarret forkert" hvis svaret er false*/
-  let myAnswer = myAnsverInput.value;
-  console.log('Answer is: ' + myAnswer);
-});
+// myAnsverButton.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   /* din kode her. Skriv en alert der skriver "du har svarret rigtigt" hvis svarret er true
+//   og "du har svarret forkert" hvis svaret er false*/
+//   if(myAnsverInput.value==="true"){
+//     alert('Du har svaret rigtigt');
+//   } else{
+//     alert('Du har svaret forkert')
+//   }
+//   let myAnswer = myAnsverInput.value;
+//   console.log('Answer is: ' + myAnswer);
+// });
 
 /*Opgave 2: udkommenter opgave 1, og omskriv nu din funktion til to korrekte svar:
 nej
 ja i disney film
 */
+myAnsverButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  /* din kode her. Skriv en alert der skriver "du har svarret rigtigt" hvis svarret er true
+  og "du har svarret forkert" hvis svaret er false*/
+  if(myAnsverInput.value==="true" || myAnsverInput.value=="also true"){
+    alert('Du har svaret rigtigt');
+  } else{
+    alert('Du har svaret forkert')
+  }
+  let myAnswer = myAnsverInput.value;
+  console.log('Answer is: ' + myAnswer);
+});
+
 
 
 /*opgave 3: skriv en kode der valider formen med følgende elementer: (denne opgave er på extreme niveau) 
@@ -33,6 +51,28 @@ ja i disney film
 - alder skal være større end 12.
  du må gerne bare consol.logge de forskellige tests i konsollen, men hvis du kan må du gerne lave feedback i elementet formValideringResult.
 */
+
+document.getElementById('myform').addEventListener('submit', function(event){
+  event.preventDefault();
+
+  let emailValue = document.getElementById('myEmail').value;
+  if(emailValue.trim().length > 3) {
+    console.log("Emailen er længere end 3 tegn og er gyldig");
+  } else {
+    alert("Emailen skal være længere end 3 tegn.");
+  }
+});
+
+document.getElementById('myform').addEventListener('submit', function(event){
+  event.preventDefault();
+
+  let ageValue = document.getElementById('myAge').value;
+  if(ageValue > 12) {
+    console.log("Alderen er højere end 12 og er gyldig");
+  } else {
+    alert("Du er ikke gammel nok");
+  }
+});
 
 
 
